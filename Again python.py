e9cbcc9e1a83c39536967e474e38ca301cda8cc6
scarsdale-sky <osuke.nyu11@gmail.com>
2026-01-28 17:12:34 +0900

    Update and rename again day1.py to Again python.py

---
+++ Again python.py
@@ -0,0 +1,19 @@
+#Day 1
+name = "Sky"
+age = 13
+print(name,age)
+print(type(name),type(age))
+
+#Day 2
+score = 86
+
+if score >= 80:
+    print("優")
+elif score >= 60:
+    print("良")
+else:
+    print("不可")
+
+#Day3
+for i in range(1,11):
+    print(i)
--- again day1.py
+++
@@ -1,4 +0,0 @@
-name = "Sky"
-age = 13
-print(name,age)
-print(type(name),type(age))
